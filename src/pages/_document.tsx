69fd4dbb7ae2403a26bcb1586c041a93499c8264
thesayf <r_ori@hotmail.co.uk>
2025-07-01 23:04:31 +0100

    added the tag manager tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PC9LS95N');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <script
           src="https://assets.calendly.com/assets/external/widget.js"
           type="text/javascript"
@@ -54,6 +67,16 @@ export default function Document() {
         />
       </Head>
       <body className="bg-white text-zinc-950">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PC9LS95N"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
